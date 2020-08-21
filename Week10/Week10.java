14cfdb1e2a13fd35a90e648cda102f1fb720c191
Vũ Đăng Huy <moonjaeshin@Vus-iMac-Pro.local>
2020-08-22 00:33:35 +0700

    do findFunctionByName unsuccessfully

--- Week10/Week10.java
+++ Week10/Week10.java
@@ -1,5 +1,23 @@
+import java.util.List;
+import java.nio.file.Files;
+import java.io.IOException;
+
 public class Week10 {
-    List<String> getAllFunctions(File path){
-        
+    List<String> getAllFunctions(File path) throws IOException{
+        List<String> result = new ArrayList<String>(); 
+        List<String> lines = Files.readAllLines(path, encoding);
+        for(int i = 0 ; i < lines.size(); i++){
+            String line = lines.get(i);
+            int index = line.indexOf('static')
+            if(index != -1){
+                result.add(line);
+            }
+        }
+        return result;
     }
+
+    public String findFunctionByName(String name) throws IOException{
+        //todo
+    }
+
 }
